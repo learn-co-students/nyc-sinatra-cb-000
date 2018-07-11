693771e9cacdc57afb335e7a430a1cd9e868fd4a
Elizabeth Jenerson <lizjen@BlerdGirlMac.fios-router.home>
2018-07-10 20:33:38 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,4 +6,16 @@ class FiguresController < ApplicationController
 		
 		erb :"figures/new"
 	end
+
+	post "/figures" do
+
+		@figure = Figure.create(params[:figure])
+
+		if !params[:landmark][:name].empty?
+			# if the Title textbox is not empty, 
+			# create a new title and add it to figure.landmark << Title.create(params[])
+		end
+
+		# redirect :"/figures/#{figure.id}"
+
 end
