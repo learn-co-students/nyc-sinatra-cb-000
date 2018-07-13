6435d157eb561f7a5b6f95c8becfee2d3efbff07
Elizabeth Jenerson <lizjen@BlerdGirlMac.fios-router.home>
2018-07-12 20:11:28 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,11 +11,23 @@ class FiguresController < ApplicationController
 
 		@figure = Figure.create(params[:figure])
 
+		# if the Title textbox is not empty, 
+		# create a new title and add it to figure.landmark << Title.create(params[])
+		
+		# if the Landmark textbox is not empty, 
+		# create a new landmark and add it to the figure's landmark array
+		# otherwise, just add the landmark.
+		# 
+		# REVIEW> THIS DOESN"T SEEM RIGHT.
+		
 		if !params[:landmark][:name].empty?
-			# if the Title textbox is not empty, 
-			# create a new title and add it to figure.landmark << Title.create(params[])
+			@figure.landmarks << Landmark.create(params[:landmark])
+		else
+			@figure.landmarks << Landmark.find_by(params[:landmark][:name])
 		end
 
-		# redirect :"/figures/#{figure.id}"
+		@figure.save
 
+		# redirect :"/figures/#{figure.id}"
+	end
 end
