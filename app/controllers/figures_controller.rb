f7489adb98c7f3efee11165d7f59129e23e72a42
Adam Urbina <adamurbina@gmail.com>
2018-09-04 21:58:42 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,53 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get "/figures/new" do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find_by(id: params[:id])
+    erb :'/figures/show'
+  end
+
+  post "/figures" do
+    # binding.pry
+    new_figure = Figure.create(name: params[:figure][:name])
+
+    if params[:figure][:title_ids]
+      params[:figure][:title_ids].each do |title_id|
+        new_figure.titles << Title.find_by(id: title_id)
+      end
+    end
+
+    if !params[:title][:name].empty?
+      new_title = Title.create(name: params[:title][:name])
+      new_figure.titles << new_title
+    end
+
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each do |landmark_id|
+        new_figure.landmarks << Landmark.find_by(id: landmark_id)
+      end
+    end
+
+    if !params[:landmark][:name].empty?
+      new_landmark = Landmark.create(name: params[:landmark][:name])
+      if !params[:landmark][:year].empty?
+        new_landmark.year_completed = params[:landmark][:year]
+        new_landmark.save
+      end
+      new_figure.landmarks << new_landmark
+    end
+
+    redirect '/figures'
+
+  end
+
 end
