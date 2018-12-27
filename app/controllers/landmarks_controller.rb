179f85a8578d4fae786f92f34a586a8c921197fc
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-12-27 20:30:06 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,6 +11,31 @@ class LandmarksController < ApplicationController
   end
 
 
-
+  post '/landmarks' do
+    change figure to landmarks
+    # @figure = Figure.create(name: params[:figure][:name])
+    #
+    # if params[:figure][:title_ids] != nil
+    #   params[:figure][:title_ids].each do |id|
+    #     @figure.titles << Title.find(id)
+    #   end
+    # end
+    #
+    # if params[:title][:name] != ""
+    #   new_title = Title.create(name: params[:title])
+    #   @figure.titles << new_title
+    # end
+    #
+    # if params[:figure][:landmark_ids] != nil
+    #   params[:figure][:landmark_ids].each do |id|
+    #     @figure.landmarks << Landmark.find(id)
+    #   end
+    # end
+    #
+    # if params[:landmark][:name] != ""
+    #   new_landmark = Landmark.create(name: params[:landmark][:name])
+    #   @figure.landmarks << new_landmark
+    # end
+  end
 
 end
